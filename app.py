e17180abc3ec204ccd6c2431a161ffdaf0c900e9
brlarce <blarce@up.edu.ph>
2021-11-29 23:03:05 +0800

    Add routes to web pages in layout.html

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
-# -- External Modules
+'''
+** app
+| This runs the Flask application.
+'''
 from flask import Flask, render_template
 
 # Redirect the application to this file.
 app = Flask(__name__)
 
-# -- Routes
-# Below is the routing to other web pages.
-
+'''
+** routes
+| This contains the routing for every page.
+'''
 # Home
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# QSI Solver
+@app.route('/solve/qsi')
+def solve_qsi():
+    return render_template('qsi.html')
+
+# Simplex Solver
+@app.route('/solve/simplex')
+def solve_simplex():
+    return render_template('simplex.html')
+
+# About SolvePy
+@app.route('/about')
+def about():
+    return render_template('about.html')
