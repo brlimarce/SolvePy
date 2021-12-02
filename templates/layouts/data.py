95074ac93d81b34566568275871be51e991b600d
brlarce <blarce@up.edu.ph>
2021-12-02 13:22:37 +0800

    Transform repetitive tags into a loop in sublayout.html

---
+++ templates/layouts/data.py
@@ -0,0 +1,84 @@
+'''
+** data
+| This is a helper file that contains
+| the data for the subpages.
+'''
+
+# ** Pages for the Navigation Bar
+pages = {
+    'q' : { 'name' : 'QSI', 'route' : '/solve/qsi' },
+    's' : { 'name' : 'Simplex', 'route' : '/solve/simplex' },
+    'a' : { 'name' : 'About', 'route' : '/about' }
+}
+
+# ** QSI
+qsi_tabs = [
+    # ** Tutorial Tab
+    { 
+        'name' : 'tutorial',
+        'header' : 'How do I use this solver? 🤔',
+        'body' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultrices et ipsum tellus. Sit mattis sem ac aliquet aenean amet nibh. Turpis eget lorem sem leo suscipit netus faucibus sed. Consectetur velit dolor sed sed. Ipsum enim laoreet odio accumsan nulla. Quam eget lorem magna id arcu. Ultricies odio feugiat semper purus sem sit magna sed gravida.'
+    },
+
+    # ** Input Tab
+    {
+        'name' : 'input',
+        'header' : 'What should I input? ⚙️',
+        'body' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultrices et ipsum tellus. Sit mattis sem ac aliquet aenean amet nibh. Turpis eget lorem sem leo suscipit netus faucibus sed. Consectetur velit dolor sed sed. Ipsum enim laoreet odio accumsan nulla. Quam eget lorem magna id arcu. Ultricies odio feugiat semper purus sem sit magna sed gravida.'
+    },
+
+    # ** Output Tab
+    {
+        'name' : 'output',
+        'header' : 'What should I receive? ✨',
+        'body' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultrices et ipsum tellus. Sit mattis sem ac aliquet aenean amet nibh. Turpis eget lorem sem leo suscipit netus faucibus sed. Consectetur velit dolor sed sed. Ipsum enim laoreet odio accumsan nulla. Quam eget lorem magna id arcu. Ultricies odio feugiat semper purus sem sit magna sed gravida.'
+    }
+]
+
+# ** Simplex
+simplex_tabs = [
+    # ** Tutorial Tab
+    { 
+        'name' : 'tutorial',
+        'header' : 'How do I use this solver? 🤔',
+        'body' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultrices et ipsum tellus. Sit mattis sem ac aliquet aenean amet nibh. Turpis eget lorem sem leo suscipit netus faucibus sed. Consectetur velit dolor sed sed. Ipsum enim laoreet odio accumsan nulla. Quam eget lorem magna id arcu. Ultricies odio feugiat semper purus sem sit magna sed gravida.'
+    },
+
+    # ** Problem Tab
+    {
+        'name' : 'problem',
+        'header' : 'What should I input? ⚙️',
+        'body' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultrices et ipsum tellus. Sit mattis sem ac aliquet aenean amet nibh. Turpis eget lorem sem leo suscipit netus faucibus sed. Consectetur velit dolor sed sed. Ipsum enim laoreet odio accumsan nulla. Quam eget lorem magna id arcu. Ultricies odio feugiat semper purus sem sit magna sed gravida.'
+    },
+
+    # ** Legend Tab
+    {
+        'name' : 'legend',
+        'header' : 'What should I receive? ✨',
+        'body' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultrices et ipsum tellus. Sit mattis sem ac aliquet aenean amet nibh. Turpis eget lorem sem leo suscipit netus faucibus sed. Consectetur velit dolor sed sed. Ipsum enim laoreet odio accumsan nulla. Quam eget lorem magna id arcu. Ultricies odio feugiat semper purus sem sit magna sed gravida.'
+    }
+]
+
+# ** About
+about_tabs = [
+    # ** Ideation Tab
+    { 
+        'name' : 'ideation',
+        'header' : 'How do I use this solver? 🤔',
+        'body' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultrices et ipsum tellus. Sit mattis sem ac aliquet aenean amet nibh. Turpis eget lorem sem leo suscipit netus faucibus sed. Consectetur velit dolor sed sed. Ipsum enim laoreet odio accumsan nulla. Quam eget lorem magna id arcu. Ultricies odio feugiat semper purus sem sit magna sed gravida.'
+    },
+
+    # ** Design Tab
+    {
+        'name' : 'design',
+        'header' : 'What should I input? ⚙️',
+        'body' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultrices et ipsum tellus. Sit mattis sem ac aliquet aenean amet nibh. Turpis eget lorem sem leo suscipit netus faucibus sed. Consectetur velit dolor sed sed. Ipsum enim laoreet odio accumsan nulla. Quam eget lorem magna id arcu. Ultricies odio feugiat semper purus sem sit magna sed gravida.'
+    },
+
+    # ** Development Tab
+    {
+        'name' : 'development',
+        'header' : 'What should I receive? ✨',
+        'body' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultrices et ipsum tellus. Sit mattis sem ac aliquet aenean amet nibh. Turpis eget lorem sem leo suscipit netus faucibus sed. Consectetur velit dolor sed sed. Ipsum enim laoreet odio accumsan nulla. Quam eget lorem magna id arcu. Ultricies odio feugiat semper purus sem sit magna sed gravida.'
+    }
+]
